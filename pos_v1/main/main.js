0126bba118e1ae8fc656395df13d10ee6e59a9ea
siu_keung@outlook.com <19950811a>
2018-07-08 11:42:45 +0800

    用新增了getGoodsList()方法，该方法整合了getUniqueIdList()方法和setPayNum()方法。

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,117 +1,128 @@
 'use strict';
 
-function getGoodsIdList(goodsIdList) {
-  let uniqueList = [];
-  for (let goodsId of goodsIdList) {
-    let temp = goodsId.split('-')[0];
-    if (uniqueList.indexOf(temp) == -1) {
-      uniqueList.push(temp);
+const fixtures = require('../test/fixtures');
+
+//返回商品:数量数组，形如：[{'123456' : 4}]
+function getGoodsList(goodsIdArray) {
+    let uniqueIdList = getUniqueIdList(goodsIdArray);
+    console.log(JSON.stringify(uniqueIdList))
+    setBuyNum(goodsIdArray, uniqueIdList);
+    return uniqueIdList;
+}
+
+function getUniqueIdList(goodsIdArray) {
+    let uniqueList = [];
+    for (let goodsId of goodsIdArray) {
+        let temp = goodsId.split('-')[0];
+        if (uniqueList.indexOf(temp) == -1) {
+            uniqueList.push(temp);
+        }
     }
-  }
-  let resultList = [];
-  uniqueList.forEach(function (item) {
-    resultList.push({id: item});
-  });
-  return resultList;
+    let resultList = [];
+    uniqueList.forEach(function (item) {
+        resultList.push({id: item});
+    });
+    return resultList;
 }
 
 function setBuyNum(idArray, goodsList) {
-  for (let goods of goodsList) {
-    let count = 0;
-    for (let idAndNum of idArray) {
-      let id = idAndNum.split('-')[0];
-      let num = 1;
-      if (idAndNum.split('-').length != 1)
-        num = parseFloat(idAndNum.split('-')[1]);
-      if (id === goods.id)
-        count += num;
+    for (let goods of goodsList) {
+        let count = 0;
+        for (let idAndNum of idArray) {
+            let id = idAndNum.split('-')[0];
+            let num = 1;
+            if (idAndNum.split('-').length != 1)
+                num = parseFloat(idAndNum.split('-')[1]);
+            if (id === goods.id)
+                count += num;
+        }
+        goods.num = count;
     }
-    goods.num = count;
-  }
 }
 
 function loadGoodsDetails(goodsList) {
-  let allGoodsList = loadAllItems();
-  for (let goods of goodsList) {
-    let goodsDetails = null;
-    for (let item of allGoodsList) {
-      if (goods.id === item.barcode) {
-        goodsDetails = item;
-        break;
-      }
+    let allGoodsList = fixtures.loadAllItems();
+    for (let goods of goodsList) {
+        let goodsDetails = null;
+        for (let item of allGoodsList) {
+            if (goods.id === item.barcode) {
+                goodsDetails = item;
+                break;
+            }
+        }
+        goods.name = goodsDetails.name;
+        goods.unit = goodsDetails.unit;
+        goods.price = goodsDetails.price;
     }
-    goods.name = goodsDetails.name;
-    goods.unit = goodsDetails.unit;
-    goods.price = goodsDetails.price;
-  }
 }
 
-function countDiscount(goodsDetails, discountList){
-    for(let discount of discountList){
+function countDiscount(goodsDetails, discountList) {
+    for (let discount of discountList) {
         //如果是买二送一活动的话
-        if(discount.type === 'BUY_TWO_GET_ONE_FREE'){
-           promote_buyTwoGetOneFree(goodsDetails, discount.barcodes);
+        if (discount.type === 'BUY_TWO_GET_ONE_FREE') {
+            promote_buyTwoGetOneFree(goodsDetails, discount.barcodes);
         }
     }
 }
 
-function promote_buyTwoGetOneFree(goodsDetails, discountBarcodes){
-  for(let barcode of discountBarcodes){
-    if(barcode === goodsDetails.id)
-      goodsDetails.payNum = goodsDetails.num - parseInt(goodsDetails.num / 3);
-  }
+function promote_buyTwoGetOneFree(goodsDetails, discountBarcodes) {
+    for (let barcode of discountBarcodes) {
+        if (barcode === goodsDetails.id)
+            goodsDetails.payNum = goodsDetails.num - parseInt(goodsDetails.num / 3);
+    }
 }
 
 function setPayNum(goodsDetailsList) {
-  for (let goodsDetails of goodsDetailsList) {
-    goodsDetails.payNum = goodsDetails.num;
-    countDiscount(goodsDetails, loadPromotions());
-  }
-  return goodsDetailsList;
+    for (let goodsDetails of goodsDetailsList) {
+        goodsDetails.payNum = goodsDetails.num;
+        countDiscount(goodsDetails, fixtures.loadPromotions());
+    }
+    return goodsDetailsList;
 }
 
 function formatReceiptStr(receipt) {
-  let total = receipt.total, discount = receipt.discount, items = receipt.items;
-  let str = '***<没钱赚商店>收据***';
-  for (let item of items) {
-    let tempStr = '\n名称：' + item.name + '，数量：' + item.num + item.unit +
-      '，单价：' + item.price.toFixed(2) + '(元)，小计：' + item.subTotal.toFixed(2) + '(元)';
-    str += tempStr;
-  }
-  str += '\n----------------------';
-  str += '\n总计：' + total.toFixed(2) + '(元)';
-  str += '\n节省：' + discount.toFixed(2) + '(元)';
-  str += '\n**********************';
-  return str;
+    let total = receipt.total, discount = receipt.discount, items = receipt.items;
+    let str = '***<没钱赚商店>收据***';
+    for (let item of items) {
+        let tempStr = '\n名称：' + item.name + '，数量：' + item.num + item.unit +
+            '，单价：' + item.price.toFixed(2) + '(元)，小计：' + item.subTotal.toFixed(2) + '(元)';
+        str += tempStr;
+    }
+    str += '\n----------------------';
+    str += '\n总计：' + total.toFixed(2) + '(元)';
+    str += '\n节省：' + discount.toFixed(2) + '(元)';
+    str += '\n**********************';
+    return str;
 }
 
-function countPrices(goodsList){
+function countPrices(goodsList) {
 
     let totalPrice = 0, totalDiscount = 0;
-    for(let goodsItem of goodsList){
+    for (let goodsItem of goodsList) {
         let subTotal = goodsItem.payNum * goodsItem.price;
         let subDiscount = goodsItem.num * goodsItem.price - subTotal;
         goodsItem.subTotal = subTotal;
         totalPrice += subTotal;
         totalDiscount += subDiscount;
     }
-    let result = {total : totalPrice, discount : totalDiscount, items : goodsList };
+    let result = {total: totalPrice, discount: totalDiscount, items: goodsList};
     console.log(JSON.stringify(result));
     return result;
 }
 
 function printReceipt(tags) {
-  let goodsList = getGoodsIdList(tags);
-  setBuyNum(tags, goodsList);
-  loadGoodsDetails(goodsList);
-  setPayNum(goodsList);
-  let result = countPrices(goodsList)
-  let formatStr = formatReceiptStr(result);
-  console.log(formatStr);
+    let goodsList = getGoodsList(tags);
+    setBuyNum(tags, goodsList);
+    loadGoodsDetails(goodsList);
+    setPayNum(goodsList);
+    let result = countPrices(goodsList)
+    let formatStr = formatReceiptStr(result);
+    console.log(formatStr);
 }
 
 module.exports = {
-  getGoodsIdList
+    getGoodsList,
+    printReceipt
 };
 
 
--- pos_v1/spec/main-spec.js
+++ pos_v1/spec/main-spec.js
@@ -10,10 +10,59 @@ const main = require('../main/main');
 //
 // });
 
-describe('main()', () => {
+describe('getGoodsList', () => {
 
-    it('should calculate the remaindar', () => {
-        expect(main.getGoodsIdList(['1', '2', '3']).length).toBe(3);
+    it('should print text', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+
+        const expectText = '[{"id":"ITEM000001","num":5},{"id":"ITEM000003","num":2.5},{"id":"ITEM000005","num":3}]';
+
+        spyOn(console, 'log');
+        console.log(JSON.stringify(main.getGoodsList(tags)));
+        expect(console.log).toHaveBeenCalledWith(expectText);
+
+    });
+
+});
+
+
+describe('printReceipt()', () => {
+
+    it('should print text', () => {
+        const tags = [
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000003-2.5',
+            'ITEM000005',
+            'ITEM000005-2',
+        ];
+
+        const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+
+        spyOn(console, 'log');
+        main.printReceipt(tags);
+        expect(console.log).toHaveBeenCalledWith(expectText);
+
     });
 
 });
